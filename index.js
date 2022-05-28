6599bc4ba5cc73500cb6ea248067f490ae5cf3f3
Siddhi <86772411+Siddhinandaniya@users.noreply.github.com>
2022-05-28 14:43:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-"strict mode";
-const btns = document.querySelectorAll(".btn");
-const box1 = document.querySelector(".box-1");
-const box2 = document.querySelector(".box-2");
-const submit = document.querySelector(".submit");
-const showRatings = document.querySelector(".rating");
+'use strict';
+
+'strict mode';
+const radiobtns = document.querySelectorAll('.radiobtn');
+const box1 = document.querySelector('.box-1');
+const box2 = document.querySelector('.box-2');
+const attri = document.querySelector('.attribution');
+const submit = document.querySelector('.submit');
+const showRatings = document.querySelector('.rating');
+
 let arr = [];
 
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    for (let j = 0; j < btns.length; j++) {
-      btns[j].classList.remove("selectedbtn");
-      btns[i].classList.add("selectedbtn");
-    }
-    console.log(btns[i].outerText);
-    arr.push(btns[i].outerText);
+for (let i = 0; i < radiobtns.length; i++) {
+  radiobtns[i].addEventListener('click', function () {
+    showRatings.outerHTML = radiobtns[i].value + ' ';
   });
 }
 
-submit.addEventListener("click", function () {
-  box1.classList.add("hidden");
-  box2.classList.remove("hidden");
-  showRatings.outerHTML = arr[arr.length - 1] + " ";
+submit.addEventListener('click', function () {
+  box1.classList.add('hidden');
+  box2.classList.remove('hidden');
+  attri.classList.add('hidden');
 });
